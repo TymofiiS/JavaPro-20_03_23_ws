37b205e581777f24f3fc2399c470820f0c854fa8
Tymofii Serhiienko <tymofii.serhiienko@gmail.com>
2023-09-04 23:43:15 +0300

    continue dev

---
+++ spring-boot-jdbc/src/main/java/ua/ithillel/hw25_7/springbootjdbc/SpringBootJdbcApplication.java
@@ -0,0 +1,13 @@
+package ua.ithillel.hw25_7.springbootjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcApplication.class, args);
+	}
+
+}
---
+++ spring-boot-jdbc/src/main/java/ua/ithillel/hw25_7/springbootjdbc/config/DataSourceConfig.java
@@ -0,0 +1,32 @@
+package ua.ithillel.hw25_7.springbootjdbc.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@EnableJdbcRepositories
+public class DataSourceConfig extends AbstractJdbcConfiguration{
+	
+	@Bean
+	public TransactionManager transactionManager(DataSource dataSource) {
+		return new JdbcTransactionManager(dataSource);
+	}
+	
+	public NamedParameterJdbcOperations namedParameterJdbcOperations(DataSource dataSource) {
+		return new NamedParameterJdbcTemplate(dataSource);
+	}
+	
+	
+}
